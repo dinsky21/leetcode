6abd2f4d3f9ba8f055654b46ebb1e3b877dbee5d
Dinsky21 <dinsky21@gmail.com>
2023-06-11 16:14:47 +0800

    finish day 8 SQL I challange

---
+++ SQL/day_8_Function/586_Customer_Placing_Largest_Number.sql
@@ -0,0 +1,30 @@
+// problem: Customer who places the most order
+
+// Example
+
+Input: 
+Orders table:
++--------------+-----------------+
+| order_number | customer_number |
++--------------+-----------------+
+| 1            | 1               |
+| 2            | 2               |
+| 3            | 3               |
+| 4            | 3               |
++--------------+-----------------+
+Output: 
++-----------------+
+| customer_number |
++-----------------+
+| 3               |
++-----------------+
+Explanation: 
+The customer with number 3 has two orders, which is greater than either customer 1 or 2 because each of them only has one order. 
+So the result is customer_number 3.
+
+// solution:
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) desc
+LIMIT 1
